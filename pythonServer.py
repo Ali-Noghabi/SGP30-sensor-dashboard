08fe1e48b1fb0dc4c4da2da917fdc2018d76523f
Ali Abdollahian Noghabi <a.noghabi2002@gmail.com>
2023-11-03 07:50:18 +0330

    Update pythonServer.py

--- pythonServer.py
+++ pythonServer.py
@@ -7,16 +7,21 @@ from datetime import datetime
 # Create or connect to the SQLite database
 conn = sqlite3.connect('sensor_data.db')
 cursor = conn.cursor()
+hostname = "localhost"
+port = 5000
 
 # Create table if it doesn't exist
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS sensor_data (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        sensor TEXT,
         timestamp TEXT,
         tvoc REAL,
         eco2 REAL,
         rawh2 REAL,
-        rawethanol REAL
+        rawethanol REAL,
+        temperature REAL,
+        humidity REAL
     )
 ''')
 
@@ -36,14 +41,17 @@ async def handle_websocket(websocket):
         else:
             try:
                 data = json.loads(message)
+                name = data["name"]
                 tvoc = data["TVOC"]
                 eco2 = data["eCO2"]
                 rawh2 = data["rawH2"]
                 rawethanol = data["rawEthanol"]
+                temperature = data["Temperature"]
+                humidity = data["Humidity"]
                 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 print(
-                    f"TVOC: {tvoc}, eCO2: {eco2}, rawH2: {rawh2}, rawEthanol: {rawethanol}, Timestamp: {timestamp}")
-                insert_data_into_database(timestamp, tvoc, eco2, rawh2, rawethanol)
+                    f"Name: {name},TVOC: {tvoc}, eCO2: {eco2}, rawH2: {rawh2}, rawEthanol: {rawethanol},Temperature: {temperature},Humidity: {humidity}, Timestamp: {timestamp}")
+                insert_data_into_database(name ,timestamp, tvoc, eco2, rawh2, rawethanol , temperature ,humidity )
             except Exception as e:
                 print(f"Error parsing message: {e}")
             await websocket.send("Message received")
@@ -53,11 +61,12 @@ def fetch_last_data_from_database():
     row = cursor.fetchone()
     if row:
         data = {
-            "TVOC": row[2],
-            "eCO2": row[3],
-            "rawH2": row[4],
-            "rawEthanol": row[5],
-            "Timestamp": row[1]
+            "Sensor":row[1],
+            "TVOC": row[3],
+            "eCO2": row[4],
+            "rawH2": row[5],
+            "rawEthanol": row[6],
+            "Timestamp": row[2]
         }
     else:
         data = {}
@@ -70,24 +79,27 @@ def fetch_data_between_timestamps(start_timestamp, end_timestamp):
     result = []
     for row in rows:
         data = {
-            "TVOC": row[2],
-            "eCO2": row[3],
-            "rawH2": row[4],
-            "rawEthanol": row[5],
-            "Timestamp": row[1]
+            "Sensor":row[1],
+            "TVOC": row[3],
+            "eCO2": row[4],
+            "rawH2": row[5],
+            "rawEthanol": row[6],
+            "Temperature": row[7],
+            "Humidity": row[8],
+            "Timestamp": row[2]
         }
         result.append(data)
     return result
 
-def insert_data_into_database(timestamp, tvoc, eco2, rawh2, rawethanol):
+def insert_data_into_database(sensor ,timestamp, tvoc, eco2, rawh2, rawethanol ,temperature , humidity):
     cursor.execute('''
-        INSERT INTO sensor_data (timestamp, tvoc, eco2, rawh2, rawethanol)
-        VALUES (?, ?, ?, ?, ?)
-    ''', (timestamp, tvoc, eco2, rawh2, rawethanol))
+        INSERT INTO sensor_data (sensor ,timestamp, tvoc, eco2, rawh2, rawethanol, temperature, humidity)
+        VALUES (? ,?, ?, ?, ?, ? , ? , ?)
+    ''', (sensor , timestamp, tvoc, eco2, rawh2, rawethanol , temperature , humidity))
     conn.commit()
 
 async def main():
-    async with websockets.serve(handle_websocket, "192.168.1.160", 5000):
+    async with websockets.serve(handle_websocket, hostname, port):
         print("Server started")
         await asyncio.Future()
 
